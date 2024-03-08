1a46ae244adb1448159cd76bd3ab0d67a6cc6490
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-03-08 19:16:00 +0530

    design search user page

--- app/src/main/java/com/example/connect/SearchUserActivity.kt
+++ app/src/main/java/com/example/connect/SearchUserActivity.kt
@@ -1,20 +1,56 @@
 package com.example.connect
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.connect.utils.FirebaseUtil
 
 class SearchUserActivity : AppCompatActivity() {
+
+    private lateinit var searchInput: EditText
+    private lateinit var searchButton: ImageButton
+    private lateinit var backButton: ImageButton
+    private lateinit var recyclerView: RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search_user)
+
+        searchInput = findViewById(R.id.seach_username_input)
+        searchButton = findViewById(R.id.search_user_btn)
+        backButton = findViewById(R.id.back_btn)
+        recyclerView = findViewById(R.id.search_user_recycler_view)
+
+        searchInput.requestFocus()
+
+        backButton.setOnClickListener { finish() }
+
+        searchButton.setOnClickListener {
+            val searchTerm = searchInput.getText().toString()
+            if (searchTerm.isEmpty() || searchTerm.length < 3) {
+                searchInput.error = "Invalid Username"
+                return@setOnClickListener
+            }
+            setupSearchRecyclerView(searchTerm)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun setupSearchRecyclerView(searchTerm: String) {
+
     }
 }
