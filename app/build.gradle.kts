c1b603ceab0a7c3de3e37e4fb6af399e845aae35
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-03-08 11:51:19 +0530

    Add login phone number layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.hbb20:ccp:2.7.1")
 }
--- app/src/main/java/com/example/connect/LoginPhoneNumberActivity.kt
+++ app/src/main/java/com/example/connect/LoginPhoneNumberActivity.kt
@@ -1,30 +1,20 @@
 package com.example.connect
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashActivity : AppCompatActivity() {
+class LoginPhoneNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login_phone_number)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@SplashActivity, LoginPhoneNumberActivity::class.java))
-            finish()
-        }, 1000)
-
-
     }
 }
